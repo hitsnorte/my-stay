145ac314e304306adad237ae1110ae698c6fce13
scnene23 <scnene23@gmail.com>
2025-04-02 16:56:05 +0100

    endpoints guest-profile

--- app/api/sysConectorStay/submit_guest_profile/route.js
+++ app/api/sysConectorStay/submit_guest_profile/route.js
@@ -84,11 +84,10 @@ export async function POST(request) {
     console.log("Enviando dados para:", url);
 
     // Enviar os dados para o servidor do hotel
-    const response = await axios.post(url, {
-      ...guestProfileData,  // Dados do hóspede
-      token,  // Token de reserva
-    }, {
+    const response = await axios.post(url, {}, {
       headers: {
+        ...guestProfileData,  // Enviando os dados do hóspede nos headers
+        token,  // Token de reserva
         Authorization: "q4vf9p8n4907895f7m8d24m75c2q947m2398c574q9586c490q756c98q4m705imtugcfecvrhym04capwz3e2ewqaefwegfiuoamv4ros2nuyp0sjc3iutow924bn5ry943utrjmi", // Token de autorização
         "Content-Type": "application/json",
       },
