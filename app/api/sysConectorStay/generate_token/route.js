dfde8ee82067a2ad28b69ec1975b40732470e52e
scnene23 <scnene23@gmail.com>
2025-04-08 10:01:54 +0100

    get guests by id endpoint and update

--- app/api/sysConectorStay/generate_token/route.js
+++ app/api/sysConectorStay/generate_token/route.js
@@ -67,8 +67,8 @@ export async function POST(request) {
     };
     
     const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "1h" });
-    // const link = `https://stay.mypms.pt/reservation/reservation?token=${token}`;
-    const link = `http://localhost:3000/reservation/reservation?token=${token}`;
+    const link = `https://stay.mypms.pt/reservation/reservation?token=${token}`;
+    // const link = `http://localhost:3000/reservation/reservation?token=${token}`;
 
     // Inserir no banco de dados
     const stayRecord = await prisma.stayRecords.create({
