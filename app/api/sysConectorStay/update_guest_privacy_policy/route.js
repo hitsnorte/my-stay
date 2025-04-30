4269775cdf62b2e5fd23cddae5584540915be76e
scnene23 <scnene23@gmail.com>
2025-04-30 14:21:42 +0100

    testes

--- app/api/sysConectorStay/update_guest_privacy_policy/route.js
+++ app/api/sysConectorStay/update_guest_privacy_policy/route.js
@@ -41,7 +41,7 @@ export async function POST(request) {
       headers: {
         Authorization: 'q4vf9p8n4907895f7m8d24m75c2q947m2398c574q9586c490q756c98q4m705imtugcfecvrhym04capwz3e2ewqaefwegfiuoamv4ros2nuyp0sjc3iutow924bn5ry943utrjmi',
         'Content-Type': 'application/json',
-        ProfileID: String(profileID)
+        ProfileID: profileID
       }
     });
 
